eee4ed1af1cf064b2d0028888c25264818929e2d
ribeiro-carlos <carlinhos.ars@gmail.com>
2022-06-02 00:51:53 -0300

    efeito maquina escrever no h1

--- js/script.js
+++ js/script.js
@@ -21,3 +21,12 @@ btnMobile.addEventListener("touchstart", toggleMenu);
 if (window.SimpleAnime) {
   new SimpleAnime();
 }
+function typeWriter(elemento) {
+  const textoArray = elemento.innerHTML.split("");
+  elemento.innerHTML = "";
+  textoArray.forEach((letra, i) => {
+    setTimeout(() => (elemento.innerHTML += letra), 75 * i);
+  });
+}
+const titulo = document.querySelector("h1");
+typeWriter(titulo);
